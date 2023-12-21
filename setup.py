935cd9dd39a285564449b82378ec340601d4f35a
Joaquin Zepeda <jazs.pro@gmail.com>
2023-12-21 12:53:17 -0800

    Minor

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
     packages=find_packages(".", exclude=["tests"]),
     version="0.1.0",
     description="Staging package for common tools",
-    install_requires=[],
+    install_requires=["pygments"],
     author="Joaquin Zepeda",
 )
